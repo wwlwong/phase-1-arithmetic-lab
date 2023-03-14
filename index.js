8f88df5bfde8f21fef6b58d13508667ecb629e49
William Wong <william.wai.lim.wong@gmail.com>
2023-03-14 14:41:36 -0400

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1,5,10,20);
